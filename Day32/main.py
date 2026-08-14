92d764de197c3a49f555b6513b42911dbcac59bb
Mohammadreza <mdavari3595@gmail.com>
2026-08-14 20:48:58 +0330

    32

---
+++ Day32/main.py
@@ -0,0 +1,20 @@
+import smtplib
+import datetime as dt
+import random
+
+MY_EMAIL = ""
+MY_PASSWORD = ""
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 4:
+    with open('quotes.txt') as quotes_file:
+        all_quotes = quotes_file.readlines()
+        quote = random.choice(all_quotes)
+
+    with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL,
+                            to_addrs="mddavari.o35@gmail.com",
+                            msg=f"Subject:Monday Motivation\n\n{quote}")
---
+++ Day32/smtp_and_datetime.py
@@ -0,0 +1,42 @@
+# import smtplib
+
+# my_email = ""
+# password = ""
+
+# connection = smtplib.SMTP('smtp.gmail.com', 587)
+# connection.starttls()
+# connection.login(user=my_email, password = password)
+# connection.sendmail(from_addr = my_email, to_addrs = "mddavari.o35@gmail.com",
+#                     msg = "Subject:Hello World\n\n"
+#                           "This is body of my email.")
+# connection.close()
+
+#same Up
+
+# with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password = password)
+#     connection.sendmail(from_addr = my_email, to_addrs = "mddavari.o35@gmail.com",
+#                         msg = "Subject:Davari\n\n"
+#                               "Jadid_ya_Qadim ?")
+
+
+#-------------------datetime--------------------
+import datetime as dt
+
+now = dt.datetime.now()
+
+year = now.year
+if year == 2026:
+    print("is 2025")
+
+month = now.month
+
+day_of_week = now.weekday()
+
+print(day_of_week)
+
+date_of_birth = dt.date(year= 1995 ,month= 12 ,day= 15)
+print(date_of_birth)
+
+#----------------------------------------
